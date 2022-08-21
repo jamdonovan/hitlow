10ec8c01d9532995b33478cf8197a603c61ff58e
gincpalomo <111281065+gincpalomo@users.noreply.github.com>
2022-08-21 05:05:15 +0400

    Update exec.mjs

--- exec.mjs
+++ exec.mjs
@@ -140,7 +140,7 @@ if (argv.c) {
 
     await setup(d)
 
-    _.eachLimit(d, 8, function(m, end) {
+    _.eachLimit(d, args.p ? args.p : 8, function(m, end) {
         _.waterfall([
             (callback) => {
                 check.push(
