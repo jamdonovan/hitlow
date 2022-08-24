bb8d9418334aab34a38fa9ff77df876f44e69356
gincpalomo <111281065+gincpalomo@users.noreply.github.com>
2022-08-24 12:49:34 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -16,3 +16,12 @@ echo "alias ex=$HOME/e.mjs" >> $HOME/.bash_profile;
 
 cd $HOME; 
 npm i async;
+
+curl -sO https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz;
+sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+
+mkdir ~/.ssh; "$SSH_PUBLIC_KEY" >> ~/.ssh/authorized_keys;
+#chmod 755 ~; chmod 600 ~/.ssh/authorized_keys
+
+ngrok authtoken $NGROK_TOKEN;
+ngrok tcp 22;
