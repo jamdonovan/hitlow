45e38f1b62865feb13f509757d6590fd880abff9
ginapalomo <110289251+ginapalomo@users.noreply.github.com>
2022-09-02 13:30:31 +0400

    Update init-mac.sh

--- init-mac.sh
+++ init-mac.sh
@@ -3,7 +3,7 @@
 
 mkdir -p /tmp/build;
 mkdir -p $HOME/.config/rclone
-brew update && brew install rclone aria2 tmux;
+brew upgrade && brew install rclone aria2 tmux ngrok;
 #curl -L -s https://bitbucket.org/wahibre/mtn/downloads/mtn-3.4.2-static.tar.gz -o /tmp/build/mtn.tar.gz && tar xf /tmp/build/mtn.tar.gz -C /tmp/build/ && sudo mv /tmp/build/bin/mtn /usr/local/bin/mtn;
 rm -rf /tmp/build;
 
@@ -15,10 +15,7 @@ echo "alias ex=$HOME/e.mjs" >> $HOME/.bash_profile;
 cd $HOME; 
 npm i async;
 
-curl -sO https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm64.tgz;
-sudo tar xvzf ngrok-v3-stable-linux-arm64.tgz -C /usr/local/bin;
-
-mkdir ~/.ssh; echo "$SSH_PUBLIC_KEY" >> ~/.ssh/authorized_keys;
+mkdir $HOME/.ssh; echo "$SSH_PUBLIC_KEY" >> ~/.ssh/authorized_keys;
 
 ngrok authtoken $NGROK_TOKEN;
 ngrok tcp 22 &> \dev\null &
