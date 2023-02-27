0c70161d29922d17e00977d10ad39b2b353c82ce
jamdonovan <126286767+jamdonovan@users.noreply.github.com>
2023-02-27 10:58:17 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@ npm i async;
 curl -sO https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz;
 sudo tar xvzf ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin;
 
-mkdir ~/.ssh; echo "$SSH_PUBLIC_KEY" >> ~/.ssh/authorized_keys;
+mkdir ~/.ssh; echo "$SSH_PUBKEY" >> ~/.ssh/authorized_keys;
 
 ngrok authtoken $NGROK_TOKEN &> /dev/null;
 ngrok tcp 22 &> /dev/null &
